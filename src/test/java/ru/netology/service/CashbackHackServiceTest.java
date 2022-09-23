1a9cedf82c055edd929d0bb5c5bb671c7664d54d
MaksimNosov <m.s.nosov@gmail.com>
2022-09-23 16:06:04 +0300

    Task 1 Part 1

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,47 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
+
+
+    CashbackHackService hackService = new CashbackHackService();
 
     @Test
-    void remain() {
+    public void testRemain() {
+
+        assertEquals(100,hackService.remain(900));
+    }
+
+    @Test
+    public void testRemain2() {
+
+        assertEquals(1,hackService.remain(999));
+    }
+
+    @Test
+    public void testRemain3() {
+
+        assertEquals(999,hackService.remain(1_001));
+    }
+
+    @Test
+    public void testRemain4() {
+
+        assertEquals(1_000,hackService.remain(1_000));
+    }
+
+    @Test
+    public void testRemain5() {
+
+        assertEquals(1_000,hackService.remain(0));
+    }
+
+    @Test
+    public void testRemain6() {
+
+        assertEquals(1_100,hackService.remain(-100));
     }
 }
